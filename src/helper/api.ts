0e2cc80bb39c17ef9fc7f6495f2188f09210c148
Pagon Game Dev <pagongamedev@gmail.com>
2023-01-08 03:42:49 +0700

    add try catch in promise

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -3,14 +3,18 @@ import { TypeGolangResponse } from './type';
 export const GolangResponse = async (
   promiseAPI: any
 ): Promise<TypeGolangResponse> => {
-  return promiseAPI
-    .then((res: any) => {
-      return { res: res.data, error: null };
-    })
-    .catch((error: any) => {
-      if (error.response && typeof error.response.data == 'object') {
-        return { res: error.response.data, error: error };
-      }
-      return { res: null, error: error };
-    });
+  try {
+    return promiseAPI
+      .then((res: any) => {
+        return { res: res.data, error: null };
+      })
+      .catch((error: any) => {
+        if (error.response && typeof error.response.data == 'object') {
+          return { res: error.response.data, error: error };
+        }
+        return { res: null, error: error };
+      });
+  } catch (error) {
+    return { res: null, error: error };
+  }
 };
--- src/helper/promise.ts
+++ src/helper/promise.ts
@@ -3,11 +3,15 @@ import { TypeGolangResponse } from './type';
 export const GolangResponse = async (
   promiseAPI: any
 ): Promise<TypeGolangResponse> => {
-  return promiseAPI
-    .then((res: any) => {
-      return { res: res, error: null };
-    })
-    .catch((error: any) => {
-      return { res: null, error: error };
-    });
+  try {
+    return promiseAPI
+      .then((res: any) => {
+        return { res: res, error: null };
+      })
+      .catch((error: any) => {
+        return { res: null, error: error };
+      });
+  } catch (error) {
+    return { res: null, error: error };
+  }
 };
